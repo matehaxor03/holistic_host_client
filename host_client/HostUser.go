fde41445e3bf35fa8106f1f19b29cb9583e4a60d
matehaxor03 <matehaxor03>
2023-11-19 09:53:48 +1100

    add exists function

--- host_client/HostUser.go
+++ host_client/HostUser.go
@@ -11,6 +11,7 @@ type HostUser struct {
 	Validate func() []error
 	Create func() []error
 	Delete func() []error
+	Exists func() (*bool, []error)
 }
 
 func newHostUser(username string) (*HostUser, []error) {
@@ -49,7 +50,49 @@ func newHostUser(username string) (*HostUser, []error) {
 		return nil
 	}
 
+	exists := func() (*bool, []error) {
+		var errors []error
+		shell_command := "dscl . read /Users/" + getUsername() + " RecordName"
+		std_outs, std_errors := bashCommand.ExecuteUnsafeCommandUsingFilesWithoutInputFile(shell_command)
+		
+		var result *bool
+		if std_errors != nil {
+			errors = append(errors, std_errors...)
+		}
+
+		if len(errors) > 0 {
+			for _, err := range errors {
+				if strings.Contains(fmt.Sprintf("%s", err), "RecordNotFound") {
+					*result = false
+					return result, nil
+				}
+			}
+
+			return nil, errors
+		} else {
+			for _, std_out := range std_outs {
+				if strings.Contains(std_out, "RecordName:") {
+					*result = true
+					return result, nil
+				}
+			}
+
+			errors = append(errors, fmt.Errorf("unable to determine if the user exists or not"))
+			return nil, errors
+		}
+	}
+
 	create := func() []error {
+		exists, exists_error := exists()
+		if exists_error != nil {
+			return exists_error
+		}
+
+		if *exists {
+			return nil
+		}
+
+
 		shell_command := "dscl . -create /Users/" + getUsername()
 		_, std_error := bashCommand.ExecuteUnsafeCommandUsingFilesWithoutInputFile(shell_command)
 		if std_error != nil {
@@ -59,6 +102,15 @@ func newHostUser(username string) (*HostUser, []error) {
 	}
 
 	delete := func() []error {
+		exists, exists_error := exists()
+		if exists_error != nil {
+			return exists_error
+		}
+
+		if !*exists {
+			return nil
+		}
+
 		shell_command := "dscl . -delete /Users/" + getUsername()
 		_, std_error := bashCommand.ExecuteUnsafeCommandUsingFilesWithoutInputFile(shell_command)
 		if std_error != nil {
@@ -73,6 +125,9 @@ func newHostUser(username string) (*HostUser, []error) {
 		},
 		Create: func() []error {
 			return create()
+		},
+		Exists: func() (*bool, []error) {
+			return exists()
 		},
 		Delete: func() []error {
 			return delete()
