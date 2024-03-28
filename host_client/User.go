f594ded04a76623839037727474e2595fd0b6350
matehaxor03 <matehaxor03>
2024-03-28 19:27:31 +1100

    make user execute query

--- host_client/User.go
+++ host_client/User.go
@@ -7,6 +7,13 @@ import (
 	"strings"
 	"fmt"
 	"strconv"
+	"time"
+	"sync"
+	"io/ioutil"
+	"os"
+	"bufio"
+	"container/list"
+	"os/exec"
 )
 
 type User struct {
@@ -29,11 +36,20 @@ type User struct {
 	GetPrimaryGroupId func() (*uint64, []error)
 	GetPrimaryGroup func() (*Group, []error)
 	SetPassword func(password string) []error
+	ExecuteUnsafeCommandUsingFiles func(command string, command_data string) ([]string, []error)
 }
 
 func newUser(username string) (*User, []error) {
 	bashCommand := common.NewBashCommand()
 	verify := validate.NewValidator()
+	const maxCapacity = 10*1024*1024  
+	delete_files := list.New()
+	lock := &sync.RWMutex{}
+	status_lock := &sync.RWMutex{}
+	file_lock := &sync.RWMutex{}
+	var wg sync.WaitGroup
+	wakeup_lock := &sync.Mutex{}
+	status := "running"
 	var this_username string
 	var this_home_directory *AbsoluteDirectory
 
@@ -411,6 +427,101 @@ func newUser(username string) (*User, []error) {
 		return nil
 	}
 
+	get_or_set_status := func(s string) string {
+		status_lock.Lock()
+		defer status_lock.Unlock()
+		if s == "" {
+			return status
+		} else {
+			status = s
+			return ""
+		}
+	}
+
+	execute_unsafe_command_simple := func(command string) ([]error) {
+		var errors []error
+		cmd := exec.Command("bash", "-c", command)
+		
+		start_error := cmd.Start()
+		if start_error != nil {
+			errors = append(errors, start_error)
+		}
+		
+		wait_error := cmd.Wait()
+		if wait_error != nil {
+			errors = append(errors, wait_error)
+		}
+
+		if len(errors) > 0 {
+			return errors
+		}
+
+		return nil
+	}
+
+	wakeup_delete_file_processor := func() {
+		wakeup_lock.Lock()
+		defer wakeup_lock.Unlock()
+		if get_or_set_status("") == "paused" {
+			get_or_set_status("try again") 
+			wg.Done()
+		} else {
+			get_or_set_status("try again") 
+		}
+	}
+
+
+	get_or_set_files := func(absolute_path_filename *string, mode string) (*string, error) {
+		file_lock.Lock()
+		defer file_lock.Unlock()
+		if mode == "push" {
+			if absolute_path_filename == nil {
+				return nil, fmt.Errorf("absolute_path_filename is nil")
+			}
+			delete_files.PushFront(absolute_path_filename)
+			wakeup_delete_file_processor()
+			return nil, nil
+		} else if mode == "pull" {
+			message := delete_files.Front()
+			if message == nil {
+				return nil, nil
+			}
+			delete_files.Remove(message)
+			return message.Value.(*string), nil
+		} else {
+			return nil, fmt.Errorf("mode is not supported %s", mode)
+		}
+	}
+
+
+	cleanup_files := func(input_file string, stdout_file string, std_err_file string) {
+		if input_file != "" {
+			get_or_set_files(&input_file, "push")
+		}
+		get_or_set_files(&stdout_file, "push")
+		get_or_set_files(&std_err_file, "push")
+	}
+
+
+	process_clean_up := func() {
+		for {
+			get_or_set_status("running")
+			time.Sleep(1 * time.Nanosecond) 
+			absolute_path_filename_to_delete, absolute_path_filename_to_delete_error := get_or_set_files(nil, "pull")
+			if absolute_path_filename_to_delete_error != nil {
+				fmt.Println(absolute_path_filename_to_delete_error)
+			} else if absolute_path_filename_to_delete != nil {
+				os.Remove(*absolute_path_filename_to_delete)
+			} else if get_or_set_status("") == "running" {
+				wg.Add(1)
+				get_or_set_status("paused")
+				wg.Wait()
+				get_or_set_status("running")
+			}
+		}
+	}
+
+
 	x := User{
 		Validate: func() []error {
 			return validate()
@@ -469,6 +580,73 @@ func newUser(username string) (*User, []error) {
 		GetDirectoryDBAbsoluteDirectory: func() (*AbsoluteDirectory, []error) {
 			return getDirectoryDBAbsoluteDirectory()
 		},
+		ExecuteUnsafeCommandUsingFiles: func(command string, command_data string) ([]string, []error) {
+			lock.Lock()
+			defer lock.Unlock()
+			var errors []error
+			var stdout_lines []string
+
+			io_absolute_directory, io_absolute_directory_errors := getDirectoryIOAbsoluteDirectory()
+			if io_absolute_directory_errors != nil {
+				return stdout_lines, io_absolute_directory_errors
+			}
+
+			directory := io_absolute_directory.GetPathAsString()
+			uuid, _ := ioutil.ReadFile("/proc/sys/kernel/random/uuid")
+			time_now := time.Now().UnixNano()
+			filename := directory + "/" + fmt.Sprintf("%v%s.sql", time_now, string(uuid))
+			filename_stdout := directory + "/" + fmt.Sprintf("%v%s-stdout.sql", time_now, string(uuid))
+			filename_stderr := directory + "/" + fmt.Sprintf("%v%s-stderr.sql", time_now, string(uuid))
+			defer cleanup_files(filename, filename_stdout, filename_stderr)
+
+			ioutil.WriteFile(filename, []byte(command_data), 0600)
+			full_command := command + " < " + filename +  " > " + filename_stdout + " 2> " + filename_stderr + " | touch " + filename_stdout + " && touch " + filename_stderr
+			execute_unsafe_command_simple(full_command)
+
+			if _, opening_stdout_error := os.Stat(filename_stdout); opening_stdout_error == nil {
+				file_stdout, file_stdout_errors := os.Open(filename_stdout)
+				if file_stdout_errors != nil {
+					errors = append(errors, file_stdout_errors)
+				} else {
+					defer file_stdout.Close()
+					stdout_scanner := bufio.NewScanner(file_stdout)
+					stdout_scanner_buffer := make([]byte, maxCapacity)
+					stdout_scanner.Buffer(stdout_scanner_buffer, maxCapacity)
+					stdout_scanner.Split(bufio.ScanLines)
+					for stdout_scanner.Scan() {
+						current_text := stdout_scanner.Text()
+						if current_text != "" {
+							stdout_lines = append(stdout_lines, current_text)
+						}
+					}
+				}
+			}
+
+			if _, opening_stderr_error := os.Stat(filename_stderr); opening_stderr_error == nil {
+				file_stderr, file_stderr_errors := os.Open(filename_stderr)
+				if file_stderr_errors != nil {
+					errors = append(errors, file_stderr_errors)
+				} else {
+					defer file_stderr.Close()
+					stderr_scanner := bufio.NewScanner(file_stderr)
+					stderr_scanner_buffer := make([]byte, maxCapacity)
+					stderr_scanner.Buffer(stderr_scanner_buffer, maxCapacity)
+					stderr_scanner.Split(bufio.ScanLines)
+					for stderr_scanner.Scan() {
+						current_text := stderr_scanner.Text()
+						if current_text != "" {
+							errors = append(errors, fmt.Errorf("%s", current_text))
+						}
+					}
+				}
+			}
+
+			if len(errors) > 0 {
+				return stdout_lines, errors
+			}
+
+			return stdout_lines, nil
+		},
 	}
 	setUsername(username)
 
@@ -477,6 +655,9 @@ func newUser(username string) (*User, []error) {
 	if errors != nil {
 		return nil, errors
 	}
+
+	go process_clean_up()
+
 
 	return &x, nil
 }
